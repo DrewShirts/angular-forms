6bdd724df947d09a0670c1c842692519f02ba6ed
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-03 14:12:41 -0700

    Form Basics In Angular_Creating a Form Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormComponent
   ],
   imports: [
     BrowserModule,
