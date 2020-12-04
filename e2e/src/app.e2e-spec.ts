dcf506b8e2364fd2053e1954e00f0bd24562b334
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-03 22:48:29 -0700

    Renamed project to angular-forms

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ps-demo app is running!');
+    expect(page.getTitleText()).toEqual('angular-forms app is running!');
   });
 
   afterEach(async () => {
