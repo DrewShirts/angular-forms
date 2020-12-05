3ded956f5f97b7a49d3eaed1120f0851d5f5388f
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-04 22:33:05 -0700

    Data Binding in Angular Forms_Creating a Data Model

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,8 @@
+
+export interface UserSettings {
+  name: string,
+  emailOffers: boolean,
+  interfaceStyle: string,
+  subscriptionType: string,
+  notes: string
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserSettings} from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  userSettings : UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
   constructor() { }
 
   ngOnInit(): void {
