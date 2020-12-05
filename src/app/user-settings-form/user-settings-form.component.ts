122ea866d4dc7e3a083c5b72c49507c76b71c9aa
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-04 23:07:15 -0700

    Data Binding in Angular Forms_Copying Form Data

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -8,13 +8,14 @@ import {UserSettings} from '../data/user-settings';
 })
 export class UserSettingsFormComponent implements OnInit {
 
-  userSettings : UserSettings = {
+  originalUserSettings : UserSettings = {
     name: 'Milton',
     emailOffers: true,
     interfaceStyle: 'dark',
     subscriptionType: 'Annual',
     notes: 'here are some notes...'
   };
+  userSettings : UserSettings = { ...this.originalUserSettings };
   constructor() { }
 
   ngOnInit(): void {
