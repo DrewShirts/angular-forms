a5e6ba447fd3b3a84d7501c2fa1a01f5c9d00fd2
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-04 23:17:37 -0700

    Form Validation_HTML5 Field Validation

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -9,11 +9,11 @@ import {UserSettings} from '../data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   originalUserSettings : UserSettings = {
-    name: 'Milton',
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: 'here are some notes...'
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   };
   userSettings : UserSettings = { ...this.originalUserSettings };
   constructor() { }
