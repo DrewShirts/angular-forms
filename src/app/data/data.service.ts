4ce980b8c83b804fb7607a44f26364c491d8a113
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-05 00:12:33 -0700

    HTTP Form Posting and Data Access_Creating a Data Service

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {UserSettings} from './user-settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  postUserSettingsForm(userSettings: UserSettings) {
+    
+  }
+}
