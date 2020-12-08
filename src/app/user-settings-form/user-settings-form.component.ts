32a9c233d69ad5d15e359e1ae446d5ca6647a191
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-08 11:36:48 -0700

    Third-party Form Controls_Working with Buttons

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -43,17 +43,17 @@ export class UserSettingsFormComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log('in onSubmit: ', form.valid);
+    console.log('in onSubmit: ', form.value);
 
-    if (form.valid) {
-      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-        (result) => console.log('success: ', result),
-        (error) => this.onHttpError(error)
-      );
-    } else {
-      this.postError = true;
-      this.postErrorMessage = 'Please fix the above errors';
-    }
+    // if (form.valid) {
+    //   this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+    //     (result) => console.log('success: ', result),
+    //     (error) => this.onHttpError(error)
+    //   );
+    // } else {
+    //   this.postError = true;
+    //   this.postErrorMessage = 'Please fix the above errors';
+    // }
   }
 
 }
